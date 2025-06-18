c8a72771b1139b0930287394cf0c27802df7e19d
Mayuresh Manchkar <mayureshmanchkar@gmail.com>
2025-06-18 20:24:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generateRoomCode(length: number = 5): string {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
